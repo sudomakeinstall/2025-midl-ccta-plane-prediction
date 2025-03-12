ee7453c6ae2b3f5e96eb893edaede61dbb3b1776
Davis Vigneault <davis.vigneault@gmail.com>
2025-03-12 16:13:24 -0700

    Update Experiments for Rebuttal

--- run-all.sh
+++ run-all.sh
@@ -1,82 +1,65 @@
 #!/usr/bin/env bash
 
-echo "Hidden 64"
+readonly EXPERIMENTS=(
+AtnY-ResY-CsdY-IndY-DirY-Hdn-64.toml
+AtnY-ResY-CsdY-IndY-DirY-Hdn-64-64.toml
+AtnY-ResY-CsdY-IndY-DirY-Hdn-128.toml
+AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128.toml
+AtnY-ResY-CsdY-IndY-DirY-Hdn-256.toml
+AtnY-ResY-CsdY-IndY-DirY-Hdn-256-256.toml
+AtnN-ResY-CsdY-IndY-DirY-Hdn-128-128.toml
+AtnY-ResN-CsdY-IndY-DirY-Hdn-128-128.toml
+AtnY-ResY-CsdN-IndY-DirY-Hdn-128-128.toml
+AtnY-ResY-CsdY-IndN-DirY-Hdn-128-128.toml
+AtnY-ResY-CsdY-IndY-DirN-Hdn-128-128.toml
+AtnY-ResY-CsdY-IndY-DirY-HdnN.toml
+AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-NO-FINE.toml
+AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-2CH.toml
+AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-3CH.toml
+AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-4CH.toml
+AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-SAX.toml
+)
+
+for e in ${EXPERIMENTS[*]};
+do
+
+echo ${plane}
+
 ./run-ccta-net.py \
-    ./experiments/midl-2025/AtnY-ResY-CsdY-Hdn-64.toml \
+    ./experiments/midl-2025/${e} \
     --train
 
-echo "Hidden 64-64"
 ./run-ccta-net.py \
-    ./experiments/midl-2025/AtnY-ResY-CsdY-Hdn-64-64.toml \
+    ./experiments/midl-2025/${e} \
+    --load-checkpoint=last \
+    --test
+
+done
+
+./run-ccta-net.py \
+    ./experiments/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-COARSE.toml \
     --train
 
-echo "Hidden 128"
-./run-ccta-net.py \
-    ./experiments/midl-2025/AtnY-ResY-CsdY-Hdn-128.toml \
-    --train
-
-echo "Hidden 128-128"
-./run-ccta-net.py \
-    ./experiments/midl-2025/AtnY-ResY-CsdY-Hdn-128-128.toml \
-    --train
-
-echo "Hidden 256"
-./run-ccta-net.py \
-    ./experiments/midl-2025/AtnY-ResY-CsdY-Hdn-256.toml \
-    --train
-
-echo "Hidden 256-256"
-./run-ccta-net.py \
-    ./experiments/midl-2025/AtnY-ResY-CsdY-Hdn-256-256.toml \
-    --train
-
-echo "No Attention"
-./run-ccta-net.py \
-    ./experiments/midl-2025/AtnN-ResY-CsdY-Hdn-128-128.toml \
-    --train
-
-echo "No Residual"
-./run-ccta-net.py \
-    ./experiments/midl-2025/AtnY-ResN-CsdY-Hdn-128-128.toml \
-    --train
-
-echo "No Cascading"
-./run-ccta-net.py \
-    ./experiments/midl-2025/AtnY-ResY-CsdN-Hdn-128-128.toml \
-    --train
-
-echo "Non-End-to-End: Coarse"
-./run-ccta-net.py \
-    ./experiments/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-COARSE.toml \
-    --train
-
-echo "Non-End-to-End: Transform"
-mkdir -p ~/projects/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-TRANSFORM/
+mkdir -p ~/projects/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-TRANSFORM/
 cp \
-    ~/projects/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-COARSE/checkpoint-last.pth.tar \
-    ~/projects/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-TRANSFORM/checkpoint-pretrain.pth.tar
+    ~/projects/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-COARSE/checkpoint-last.pth.tar \
+    ~/projects/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-TRANSFORM/checkpoint-pretrain.pth.tar
 cp \
-    ~/projects/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-COARSE/data.csv \
-    ~/projects/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-TRANSFORM/data.csv
+    ~/projects/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-COARSE/data.csv \
+    ~/projects/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-TRANSFORM/data.csv
 
 ./run-ccta-net.py \
-    ./experiments/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-TRANSFORM.toml \
+    ./experiments/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-TRANSFORM.toml \
     --load-checkpoint=pretrain \
     --train
 
-echo "Non-End-to-End: Fine"
-mkdir -p ~/projects/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-FINE/
+mkdir -p ~/projects/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-FINE/
 cp \
-    ~/projects/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-TRANSFORM/checkpoint-last.pth.tar \
-    ~/projects/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-FINE/checkpoint-pretrain.pth.tar
+    ~/projects/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-TRANSFORM/checkpoint-last.pth.tar \
+    ~/projects/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-FINE/checkpoint-pretrain.pth.tar
 cp \
-    ~/projects/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-TRANSFORM/data.csv \
-    ~/projects/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-FINE/data.csv
+    ~/projects/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-TRANSFORM/data.csv \
+    ~/projects/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-FINE/data.csv
 ./run-ccta-net.py \
-    ./experiments/midl-2025/AtnY-ResY-CsdY-Hdn-128-128-FINE.toml \
-    --train
-
-echo "No Hidden"
-./run-ccta-net.py \
-    ./experiments/midl-2025/AtnY-ResY-CsdY-HdnN.toml \
+    ./experiments/midl-2025/AtnY-ResY-CsdY-IndY-DirY-Hdn-128-128-FINE.toml \
     --train
